5850395e8bb4599aefecd930329e790c03abc37a
Samer taha <alrazy1@gmail.com>
2023-01-04 18:29:07 +0200

    added autherization functionality

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,7 +3,9 @@ import * as api from '../api'
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
-    // log in the user
+    const { data } = await api.signIn(formData)
+
+    dispatch({ type: AUTH, data })
 
     history.push('/')
   } catch (error) {
@@ -12,7 +14,9 @@ export const signin = (formData, history) => async (dispatch) => {
 }
 export const signup = (formData, history) => async (dispatch) => {
   try {
-    // sign up the user
+    const { data } = await api.signUp(formData)
+
+    dispatch({ type: AUTH, data })
 
     history.push('/')
   } catch (error) {
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,10 +9,12 @@ import { createPost, updatePost } from '../../actions/posts'
 // get the current id of the post
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
+  const [postData, setPostData] = useState({  title: '', message: '', tags: '', selectedFile: '' })
   const post = useSelector((state) => (currentId ? state.posts.find((p) => p._id === currentId) : null))
   const classes = useStyles()
   const dispatch = useDispatch()
+  const user = JSON.parse(localStorage.getItem('profile'))
+
   console.log('ci:', currentId)
   console.log('post:', post)
   console.log('postData:', postData)
@@ -21,25 +23,35 @@ const Form = ({ currentId, setCurrentId }) => {
     if (post) setPostData({ ...post })
   }, [post])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    if (currentId) {
-      dispatch(updatePost(currentId, postData))
+
+    if (currentId === 0) {
+      dispatch(createPost({...postData,name: user?.result?.name}))
     } else {
-      dispatch(createPost(postData))
+      dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }))
     }
     clear()
   }
   const clear = () => {
-    setCurrentId(null)
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
+    setCurrentId(0)
+    setPostData({ title: '', message: '', tags: '', selectedFile: '' })
+  }
+
+  if (!user?.result?.name) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h6" align="center">
+            Please Sign In to create your own memories and like other's memories.
+        </Typography>  
+       </Paper>  
+    )
   }
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a Memorial</Typography>
-        <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
         <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name='messsage' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
