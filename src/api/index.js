09467fd878e90ccef84a709c04b6be529bee90ea
Samer taha <alrazy1@gmail.com>
2023-02-13 13:25:11 +0200

    late

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: "http://localhost:5000" })
+//const API = axios.create({ baseURL: "http://localhost:5000" })
+const API = axios.create({ baseURL: "https://deathopedia-backend.onrender.com" })
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
